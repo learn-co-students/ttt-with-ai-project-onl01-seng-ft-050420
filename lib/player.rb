582c5614fbd14ffc8b07989cfc2ccb73f23019b3
Luke Chen <luke.chen.2010@gmail.com>
2020-05-27 17:35:08 -0500

    Refactor

--- lib/player.rb
+++ lib/player.rb
@@ -22,15 +22,8 @@ class Players
     
     def move(board)
 
-      moves = minimax(board, @token, true)    
-      max_moves = []
-
-      moves.each do |key, value|
-        max_moves << key if value == moves.values.max
-      end
-      
-      move = max_moves.shuffle[0]
-      
+      move = minimax(board, @token, true)    
+    
       return (move + 1).to_s
 
     end
@@ -71,7 +64,14 @@ class Players
       end   
       
       if first_call 
-        return moves
+        max_moves = []
+
+        moves.each do |key, value|
+          max_moves << key if value == moves.values.max
+        end
+      
+        move = max_moves.shuffle[0]
+        return move
       end
 
       if next_player == @token
